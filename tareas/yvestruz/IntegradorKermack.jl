eac9476eea3fb6bd67c9bdbbf125309e5630263e
Luis Yves <yves@ciencias.unam.mx>
2016-06-03 13:04:21 -0500

    Modifico presentación y creo archivo .jl con todos los integradores

---
+++ tareas/yvestruz/IntegradorKermack.jl
@@ -0,0 +1,415 @@
+#=
+Este módulo contiene las funciones integradoras para el modelo epidemiológico de Kermack McKendrick y sus variaciones.
+
+Autores:
+Luis Yves Villegas Aguilar
+David Leonardo Galicia Praskauer
+Arturo Cid Prati
+
+03 de Junio de 2016
+=#
+
+
+__precompile__(true)
+
+module KMK
+    	import Base: +, -, *, /, ^
+	import ADT: Taylor, gradomax, prom, evaluar
+
+    	export coefs_1, paso, ev_horner, integrador_kermack, coefs_1_re, integrador_kermack_re, coefs_SIRS, integrador_kermack_SIRS, coefs_3, k_3, coefs_3_reincidencia, k_3_reincidencia
+
+
+########### SIR 1 especie
+## Coefs
+function coefs_1(gamma, beta, S0, I0, R0, n = 40)
+    s = [S0]
+    i = [I0]
+    r = [R0]
+    F1(S,I) = beta*S*I
+    F2(I) = gamma*I
+    for k = 1:n
+        Ts = Taylor(s)
+        Ti = Taylor(i)
+        Tr = Taylor(r)
+        fs = -F1(Ts,Ti)
+        fi = F1(Ts, Ti) - F2(Ti)
+        fr = F2(Ti)
+        S0 = fs.pol[k]/k
+        I0 = fi.pol[k]/k
+        R0 = fr.pol[k]/k
+        push!(s,S0)
+        push!(r,R0)
+        push!(i,I0)
+    end
+    s = Taylor(s)
+    i = Taylor(i)
+    r = Taylor(r)
+    return s,i,r    
+end
+
+
+## Paso integrador
+function paso(a::Taylor, p = 40)
+    eps_abs = 1e-30
+    while p != 0 && a.pol[p] == 0
+       p = p-1;
+    end
+    h = 0.5*(eps_abs/abs(a.pol[p]))^(1/p)
+end
+
+
+## Evaluación de Horner
+function ev_horner{T<:Number}(a::Taylor,t0::T)
+    n = gradomax(a)
+    x1 = a.pol[n]
+    for k = 1:n-1
+        x1 = a.pol[n-k] + x1*t0
+    end
+    return x1
+end
+
+
+## Integrador
+function integrador_kermack(N, I0, R0, t0, tf, gamma, beta)
+    S0 = N - I0 - R0 
+    t = [t0]
+    s = [S0]
+    i = [I0]
+    r = [R0]
+    while t0 < tf
+        S,I,R = coefs_1(gamma, beta, S0, I0, R0)
+        h_s = paso(S)
+        h_i = paso(I)
+        h_r = paso(R)
+        h = min(h_s, h_i, h_r)
+        t0 += h
+        S0 = ev_horner(S,h)
+        I0 = ev_horner(I,h)
+        R0 = ev_horner(R,h)
+        push!(t,t0)
+        push!(s,S0)
+        push!(i,I0)
+        push!(r,R0)
+    end
+    return t,s,i,r
+end
+
+
+########### SIRS
+## Coefs
+function coefs_1_re(gamma, beta, phi, S0, I0, R0, n = 40)
+    s = [S0]
+    i = [I0]
+    r = [R0]
+    F1(S,I) = beta*S*I
+    F2(I) = gamma*I
+    F3(R) = phi*R
+    for k = 1:n
+        Ts = Taylor(s)
+        Ti = Taylor(i)
+        Tr = Taylor(r)
+        fs = -F1(Ts,Ti) + F3(Tr)# F1 = -b*S*I + p*R
+        fi = F1(Ts, Ti) - F2(Ti)
+        fr = F2(Ti) - F3(Tr) # F2 = g*I-p*R
+        S0 = fs.pol[k]/k
+        I0 = fi.pol[k]/k
+        R0 = fr.pol[k]/k
+        push!(s,S0)
+        push!(r,R0)
+        push!(i,I0)
+    end
+    s = Taylor(s)
+    i = Taylor(i)
+    r = Taylor(r)
+    return s,i,r
+end
+
+
+## Integrador
+function integrador_kermack_re(N, I0, R0, t0, tf, gamma, beta, phi)
+    S0 = N - I0 - R0
+    t = [t0]
+    s = [S0]
+    i = [I0]
+    r = [R0]
+    n_iteraciones = 1
+    while t0 < tf && n_iteraciones < 10000
+        S,I,R = coefs_1_re(gamma, beta, phi, S0, I0, R0)
+        h_s = paso(S)
+        h_i = paso(I)
+        h_r = paso(R)
+        h = min(h_s, h_i, h_r)
+        t0 += h
+        S0 = ev_horner(S,h)
+        I0 = ev_horner(I,h)
+        R0 = ev_horner(R,h)
+        push!(t,t0)
+        push!(s,S0)
+        push!(i,I0)
+        push!(r,R0)
+        n_iteraciones += 1
+    end
+    return t,s,i,r
+end
+
+
+########### SIRS con nacimientos y muertes
+## Coefs
+function coefs_SIRS(γ,β,μ,f,S_0,I_0,R_0,n=50)
+    N=S_0+I_0+R_0
+    s=[S_0]
+    i=[I_0]
+    r=[R_0]   
+    F1(S,I,R) = -(β/N)*S*I + μ*(I+R)+f*R
+    F2(S,I) = (β/N)*S*I -γ*I -μ*I
+    F3(I,R) = γ*I-μ*R-f*R
+    f1=Taylor(0) 
+    f2=Taylor(0)
+    f3=Taylor(0)
+    for k in 1:n
+        Ts = Taylor(s)
+        Ti = Taylor(i)
+        Tr = Taylor(r)
+        f1=F1(Ts,Ti,Tr)
+        f2=F2(Ts,Ti)
+        f3=F3(Ti,Tr)
+        S_0 = f1.pol[k]/k
+        I_0 = f2.pol[k]/k
+        R_0 = f3.pol[k]/k
+        push!(s,S_0)
+        push!(r,R_0)
+        push!(i,I_0)
+    end
+    s = Taylor(s)
+    i = Taylor(i)
+    r = Taylor(r)
+    return s,i,r
+end
+
+
+## Integrador
+"""
+Esta función integra la ecuación diferencial del modelo de Kermack-McKendrick SIRS discurito previamente. Sus parámetros son:
+integrador_kermack(N=población total,I0=infectados inicialmente,t0=tiempo inicial,tf=tiempo final,γ=tasa de recuperación,
+β=tasa de infección,μ=tasa de natalidad,f=tasa,n=numero de términos en los pols. de Taylor). 
+Se consideran nacimientos y muertes, pero se asume que la tasa de natalidad es igual que la de mortalidad. Con esto,
+la población total se mantiene constante
+"""
+function integrador_kermack_SIRS(N, I0, R0, t0, tf, gamma, beta,mu,f,n=50)
+    S0 = N - I0 - R0
+    t = [t0]
+    s = [S0]
+    i = [I0]
+    r = [R0]
+    while t0 < tf
+        S,I,R = coefs_SIRS(gamma, beta,mu,f, S0, I0, R0,n)
+        h_s = paso(S)
+        h_i = paso(I)
+        h_r = paso(R)
+        h = min(h_s, h_i, h_r)
+        t0 += h
+        S0 = ev_horner(S,h)
+        I0 = ev_horner(I,h)
+        R0 = ev_horner(R,h)
+        push!(t,t0)
+        push!(s,S0)
+        push!(i,I0)
+        push!(r,R0)
+    end
+    return t,s,i,r
+end
+
+
+########### SIR 3 especies
+## Coefs
+function coefs_3(gamma, beta, S0_1, S0_2, S0_3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3, n = 40)
+    S = Array{Array{Float64,1},1}();
+    push!(S,[S0_1])
+    push!(S,[S0_2])
+    push!(S,[S0_3])
+    I = Array{Array{Float64,1},1}();
+    push!(I,[I0_1])
+    push!(I,[I0_2])
+    push!(I,[I0_3])
+    R = Array{Array{Float64,1},1}();
+    push!(R,[R0_1])
+    push!(R,[R0_2])
+    push!(R,[R0_3])
+    F1(beta_ij,S,I) = -beta_ij*S*I
+    F2(gamma_i,I) = gamma_i*I
+    for k = 1:n-1
+        Ts = [Taylor(S[j]) for j in 1:3]
+        Ti = [Taylor(I[j]) for j in 1:3]
+        Tr = [Taylor(R[j]) for j in 1:3]
+        for i = 1:3
+            fs = 0
+            fi = 0
+            fr = 0
+            for j = 1:3
+                fs += F1(beta[i,j],Ts[i],Ti[j])
+            end
+            fr = F2(gamma[i], Ti[i])
+            fi = -fs - fr
+            S0 = fs.pol[k]/k
+            I0 = fi.pol[k]/k
+            R0 = fr.pol[k]/k
+            push!(S[i], S0)
+            push!(I[i], I0)
+            push!(R[i], R0)
+        end
+    end
+    s = [Taylor(S[i]) for i in 1:3]
+    i = [Taylor(I[i]) for i in 1:3]
+    r = [Taylor(R[i]) for i in 1:3]
+    return s,i,r
+end
+
+
+## Integrador
+function k_3(N1, N2, N3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3, t0, tf, gamma, beta)
+    S0_1 = N1 - I0_1 - R0_1
+    S0_2 = N2 - I0_2 - R0_2
+    S0_3 = N3 - I0_3 - R0_3
+    t = [t0]
+    s = Array{Array{Float64,1},1}();
+    push!(s,[S0_1])
+    push!(s,[S0_2])
+    push!(s,[S0_3])
+    i = Array{Array{Float64,1},1}();
+    push!(i,[I0_1])
+    push!(i,[I0_2])
+    push!(i,[I0_3])
+    r = Array{Array{Float64,1},1}();
+    push!(r,[R0_1])
+    push!(r,[R0_2])
+    push!(r,[R0_3])
+    matriz_de_paso = zeros(3)
+    while t0 < tf
+        S0_1 = s[1][end]
+        S0_2 = s[2][end]
+        S0_3 = s[3][end]
+        I0_1 = i[1][end]
+        I0_2 = i[2][end]
+        I0_3 = i[3][end]
+        R0_1 = r[1][end]
+        R0_2 = r[2][end]
+        R0_3 = r[3][end]
+        S,I,R = coefs_3(gamma, beta, S0_1, S0_2, S0_3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3)
+        for k in 1:3
+            h_s = paso(S[k])
+            h_i = paso(I[k])
+            h_r = paso(R[k])
+            h = min(h_s, h_i, h_r)
+            matriz_de_paso[k] = h
+            S0 = ev_horner(S[k],h)
+            I0 = ev_horner(I[k],h)
+            R0 = ev_horner(R[k],h)
+            push!(s[k],S0)
+            push!(i[k],I0)
+            push!(r[k],R0)
+        end
+        t0 += min(matriz_de_paso...)
+        push!(t, t0)
+    end
+    return t,s,i,r
+end
+
+
+########### SIRS varias especies
+## Coefs
+function coefs_3_reincidencia(gamma, beta, phi, S0_1, S0_2, S0_3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3, n = 40)
+    S = Array{Array{Float64,1},1}();
+    push!(S,[S0_1])
+    push!(S,[S0_2])
+    push!(S,[S0_3])
+    I = Array{Array{Float64,1},1}();
+    push!(I,[I0_1])
+    push!(I,[I0_2])
+    push!(I,[I0_3])
+    R = Array{Array{Float64,1},1}();
+    push!(R,[R0_1])
+    push!(R,[R0_2])
+    push!(R,[R0_3])
+    F1(beta_ij,S,I) = -beta_ij*S*I
+    F2(gamma_i,I) = gamma_i*I
+    F3(phi_i,R) = phi_i*R
+    for k = 1:n-1
+        Ts = [Taylor(S[j]) for j in 1:3]
+        Ti = [Taylor(I[j]) for j in 1:3]
+        Tr = [Taylor(R[j]) for j in 1:3]
+        for i = 1:3
+            fs = 0
+            fi = 0
+            fr = 0
+            for j = 1:3
+                fs += F1(beta[i,j],Ts[i],Ti[j])
+            end
+            fs += F3(phi[i],Tr[i])
+            fr = F2(gamma[i], Ti[i])-F3(phi[i],Tr[i])
+            fi = -fs - fr
+            S0 = fs.pol[k]/k
+            I0 = fi.pol[k]/k
+            R0 = fr.pol[k]/k
+            push!(S[i], S0)
+            push!(I[i], I0)
+            push!(R[i], R0)
+        end
+    end
+    s = [Taylor(S[i]) for i in 1:3]
+    i = [Taylor(I[i]) for i in 1:3]
+    r = [Taylor(R[i]) for i in 1:3]
+    return s,i,r
+end
+
+## Integrador
+function k_3_reincidencia(N1, N2, N3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3, t0, tf, gamma, beta, phi)
+    S0_1 = N1 - I0_1 - R0_1
+    S0_2 = N2 - I0_2 - R0_2
+    S0_3 = N3 - I0_3 - R0_3
+    t = [t0]
+    s = Array{Array{Float64,1},1}();
+    push!(s,[S0_1])
+    push!(s,[S0_2])
+    push!(s,[S0_3])
+    i = Array{Array{Float64,1},1}();
+    push!(i,[I0_1])
+    push!(i,[I0_2])
+    push!(i,[I0_3])
+    r = Array{Array{Float64,1},1}();
+    push!(r,[R0_1])
+    push!(r,[R0_2])
+    push!(r,[R0_3])
+    matriz_de_paso = zeros(3)
+    n_iteracion = 1
+    while t0 < tf && n_iteracion < 2000
+        S0_1 = s[1][end]
+        S0_2 = s[2][end]
+        S0_3 = s[3][end]
+        I0_1 = i[1][end]
+        I0_2 = i[2][end]
+        I0_3 = i[3][end]
+        R0_1 = r[1][end]
+        R0_2 = r[2][end]
+        R0_3 = r[3][end]
+        S,I,R = coefs_3_reincidencia(gamma, beta, phi, S0_1, S0_2, S0_3, I0_1, I0_2, I0_3, R0_1, R0_2, R0_3)
+        for k in 1:3
+            h_s = paso(S[k])
+            h_i = paso(I[k])
+            h_r = paso(R[k])
+            h = min(h_s, h_i, h_r)
+            matriz_de_paso[k] = h
+            S0 = ev_horner(S[k],h)
+            I0 = ev_horner(I[k],h)
+            R0 = ev_horner(R[k],h)
+            push!(s[k],S0)
+            push!(i[k],I0)
+            push!(r[k],R0)
+        end
+        t0 += min(matriz_de_paso...)
+        push!(t, t0)
+        n_iteracion += 1
+    end
+    return t,s,i,r
+end
+
+end
