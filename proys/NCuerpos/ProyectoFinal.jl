5613e21804e9e5fd8dcd9750537b6c2abf9fb60b
Maria Fernanda Perez Ramirez <marifer_92@ciencias.unam.mx>
2016-06-06 23:50:46 -0500

    Para el problema restringido...

--- proys/NCuerpos/ProyectoFinal.jl
+++ proys/NCuerpos/ProyectoFinal.jl
@@ -13,12 +13,12 @@ function anima3C(c1,c2,c3, nombre::ASCIIString, loop)
     py1 = [x[2] for x in c1[1]]
     py2 = [x[2] for x in c2[1]]
     py3 = [x[2] for x in c3[1]]
-    
-    
+
+
     fig = figure(figsize=(10,10))
     cuadros = [[plot(px1[i],py1[i], ",", px2[i],py2[i],  ",", px3[i],py3[i], marker = ".", color ="r")] for i=1:loop:length(px1)]
-    
-    animacion = anim.ArtistAnimation(fig, cuadros, interval=200, blit=true) 
+
+    animacion = anim.ArtistAnimation(fig, cuadros, interval=200, blit=true)
     animacion[:save](nombre*".mp4", extra_args=["-vcodec", "libx264", "-pix_fmt", "yuv420p"])
 end
 
@@ -34,7 +34,7 @@ const epsilon = 1.0e-20
 const G = 1
 function paso_int{T<:Real}(x_0::Taylor1{T})
     orden = x_0.order
-    h1 = (epsilon/abs(x_0.coeffs[orden + 1]))^(1/orden) 
+    h1 = (epsilon/abs(x_0.coeffs[orden + 1]))^(1/orden)
     h2 = (epsilon/abs(x_0.coeffs[orden]))^(1/(orden - 1))
     min(h1, h2)
 end
@@ -51,7 +51,7 @@ end
 
 #function Integrador_1(pos_iniciales, vel_ini, masas::Array{Float64,1}, t0::Float64, tf::Float64, p::Int)
 function Integrador3(cond_ini, tf::Float64, p=28)
-   
+
     #Para ahorrar al momento de escribir los nombres se indicarán así: pi donde i es el número de cuerpo,
     # y p inidica si es posición o velocidad
     #Creamos los arreglos con los tiempos y las soluciones
@@ -62,14 +62,14 @@ function Integrador3(cond_ini, tf::Float64, p=28)
     p2 = typeof(zeros(Float64, 3))[[cond_ini[2][2], cond_ini[2][3], cond_ini[2][4]]]
     p3 = typeof(zeros(Float64, 3))[[cond_ini[3][2], cond_ini[3][3], cond_ini[3][4]]]
     ts = Float64[0.] #arreglo inicial para los tiempos
-    
+
     m1 = cond_ini[1][1]
     m2 = cond_ini[2][1]
     m3 = cond_ini[3][1]
-    
+
     t = 0.
     h = 1
-    
+
     while t <= tf && h > 1e-8
         #Creo arreglos de cada parámetro
         arr_x1 = Float64[p1[end][1]]
@@ -90,7 +90,7 @@ function Integrador3(cond_ini, tf::Float64, p=28)
         arr_vz1 = Float64[p1[end][3]]
         arr_vz2 = Float64[p2[end][3]]
         arr_vz3 = Float64[p3[end][3]]
-        
+
         #Creo Taylor's para cada parámetro
         for j in 1:p
             Taylor_arr_x1 = Taylor1(arr_x1)
@@ -111,17 +111,17 @@ function Integrador3(cond_ini, tf::Float64, p=28)
             Taylor_arr_vz1 = Taylor1(arr_vz1)
             Taylor_arr_vz2 = Taylor1(arr_vz2)
             Taylor_arr_vz3 = Taylor1(arr_vz3)
-            
-            #@show Taylor_arr_x1          
+
+            #@show Taylor_arr_x1
             ##Definimos la operación de las 6 ecs de movimiento.
-            
-            
+
+
 r12 = ((Taylor_arr_x1 - Taylor_arr_x2)^2 + (Taylor_arr_y1 - Taylor_arr_y2)^2 + (Taylor_arr_z1 - Taylor_arr_z2)^2)^(1/2)
 r13 = ((Taylor_arr_x1 - Taylor_arr_x3)^2 + (Taylor_arr_y1 - Taylor_arr_y3)^2 + (Taylor_arr_z1 - Taylor_arr_z3)^2)^(1/2)
 r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (Taylor_arr_z2 - Taylor_arr_z3)^2)^(1/2)
-            
-            #@show r12, r3, r23       
-            
+
+            #@show r12, r3, r23
+
             #Para el cuerpo 1
             fx1 = -G*(m2*(Taylor_arr_x1 - Taylor_arr_x2)/(r12)^2 + m3*(Taylor_arr_x1 - Taylor_arr_x3)/(r13)^2)
             fy1 = -G*(m2*(Taylor_arr_y1 - Taylor_arr_y2)/(r12)^2 + m3*(Taylor_arr_y1 - Taylor_arr_y3)/(r13)^2)
@@ -134,7 +134,7 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
             fx3 = -G*(m1*(Taylor_arr_x3 - Taylor_arr_x1)/(r13)^2 + m2*(Taylor_arr_x3 - Taylor_arr_x2)/(r23)^2)
             fy3 = -G*(m1*(Taylor_arr_y3 - Taylor_arr_y1)/(r13)^2 + m2*(Taylor_arr_y3 - Taylor_arr_y2)/(r23)^2)
             fz3 = -G*(m1*(Taylor_arr_z3 - Taylor_arr_z1)/(r13)^2 + m2*(Taylor_arr_z3 - Taylor_arr_z2)/(r23)^2)
-            
+
             #Incluimos los nuevos coeficientes
             arr_x1 = push!(arr_x1, Taylor_arr_vx1.coeffs[j]/j)
             arr_x2 = push!(arr_x2, Taylor_arr_vx2.coeffs[j]/j)
@@ -175,8 +175,8 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
         Taylor_arr_vz2 = Taylor1(arr_vz2)
         Taylor_arr_vz3 = Taylor1(arr_vz3)
 
-        
-        #Calculamos todas las h's posibles        
+
+        #Calculamos todas las h's posibles
         h1 = paso_int(Taylor_arr_x1)
         h2 = paso_int(Taylor_arr_x2)
         h3 = paso_int(Taylor_arr_x3)
@@ -192,8 +192,8 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
         h13 = paso_int(Taylor_arr_vz1)
         h14 = paso_int(Taylor_arr_vz2)
         h15 = paso_int(Taylor_arr_vz3)
-        
-        
+
+
         #Elegimos el h más pequeño
         h = min(h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, h15)
         t += h
@@ -216,10 +216,10 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
         vz1 = evaluate(Taylor_arr_vz1, h)
         vz2 = evaluate(Taylor_arr_vz2, h)
         vz3 = evaluate(Taylor_arr_vz3, h)
-        
+
         #Revisamos que no salgan singularidades
-        
-        
+
+
         #Creamos vectores para la posicion y la velocidad de cada cuerpo
         p1_temp = Float64[x1, y1, z1]
         p2_temp = Float64[x2, y2, z2]
@@ -227,7 +227,7 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
         v1_temp = Float64[vx1, vy1, vz1]
         v2_temp = Float64[vx2, vy2, vz2]
         v3_temp = Float64[vx3, vy3, vz3]
-        
+
         #Agregamos nueva información de tiempos, posiciones y velocidades
         p1 = push!(p1, p1_temp)
         p2 = push!(p2, p2_temp)
@@ -241,11 +241,11 @@ r23 = ((Taylor_arr_x2 - Taylor_arr_x3)^2 + (Taylor_arr_y2 - Taylor_arr_y3)^2 + (
     cuerpo1 = typeof(p1)[p1, v1]
     cuerpo2 = typeof(p2)[p2, v2]
     cuerpo3 = typeof(p3)[p3, v3]
-    
+
     ts, cuerpo1, cuerpo2, cuerpo3
 end
 
-function IntegradorN(cond_ini, tf, p=28)    
+function IntegradorN(cond_ini, tf, p=28)
     #Creamos símbolos y expresiones las variables internas
     #Nombres para variables de posiciones, velocidades y masas i-ésimas
     n_vj(j) = symbol(string("v", j))
@@ -278,13 +278,13 @@ function IntegradorN(cond_ini, tf, p=28)
     n_vz(j) = symbol(string("vz", j))
     #Nombre para los arreglos con toda la información
     n_c(j) = symbol(string("c", j))
-    
+
     #Nombre para todas las h
     n_h(j) = symbol(string("h",j))
-    
+
     #Determinamos el número de cuerpos
     const N = length(cond_ini)
-    
+
     #Creamos los vectores con las N posiciones y velocidades, además de tener las N masas separadas
     for j in 1:N
         #Determinamos valores iniciales
@@ -295,7 +295,7 @@ function IntegradorN(cond_ini, tf, p=28)
         vjx = cond_ini[j][5]
         vjy = cond_ini[j][6]
         vjz = cond_ini[j][7]
-        
+
         #Creamos las expresiones
         e_vj = :(typeof(zeros(Float64, 3))[[$vjx,$vjy,$vjz]])
         e_pj = :(typeof(zeros(Float64, 3))[[$pjx,$pjy,$pjz]])
@@ -307,7 +307,7 @@ function IntegradorN(cond_ini, tf, p=28)
         eval(expr2)
         eval(expr3)
     end
-    
+
     #Creamos el arreglo con los tiempos
     ts = Float64[0.0]
     t = 0.0
@@ -329,12 +329,12 @@ function IntegradorN(cond_ini, tf, p=28)
             eval(expr5)
             eval(expr6)
         end
-        
+
         #Ahora, debemos calcular los Taylor's de los parámetros usando las ecuaciones de movimiento desde orden 1
         #hasta el orden p
         for k in 1:p
             #Primero, debemos crear los Taylor's para cada parámetro
-            
+
             for j in 1:N
                 #Creamos expresiones correspondientes y usamos los datos para crear Taylor's
                 expr = :($(n_Tarrxj(j)) =  Taylor1($(n_arrxj(j))))
@@ -350,7 +350,7 @@ function IntegradorN(cond_ini, tf, p=28)
                 eval(expr5)
                 eval(expr6)
             end
-            
+
             #Ahora, debemos calcular para cada cuerpo sus 3 parámetros: x, y, z
             for j in 1:N
                 #Primero, averguamos los N-1 cuerpos con los cuales el cuerpo j tiene interacción
@@ -375,7 +375,7 @@ function IntegradorN(cond_ini, tf, p=28)
                 expx = :(($expx)*(G)*($(n_mj(j))))
                 expy = :(($expy)*(G)*($(n_mj(j))))
                 expz = :(($expz)*(G)*($(n_mj(j))))
-                
+
                 #Creamos las expresiones finales
                 expr = :($(n_fxj(j)) = $expx)
                 expr2 = :($(n_fyj(j)) = $expy)
@@ -383,7 +383,7 @@ function IntegradorN(cond_ini, tf, p=28)
                 eval(expr)
                 eval(expr2)
                 eval(expr3)
-                
+
                 #Ya calculamos el Taylor para cada parámetro, ahora usemos las relaciones de recurrencia
                 #Agreguemos a los arreglos de cada parámetro los nuevos valores
                 expr = :($(n_arrxj(j)) = push!($(n_arrxj(j)) , $(n_Tarrvxj(j)).coeffs[$k]/$k) )
@@ -398,9 +398,9 @@ function IntegradorN(cond_ini, tf, p=28)
                 eval(expr4)
                 eval(expr5)
                 eval(expr6)
-            end            
+            end
         end
-        
+
          #Ahora sí, calculemos los Taylor's de los arreglos finales para los 6N parámetros
         for j in 1:N
             #Creamos expresiones correspondientes y usamos los datos para crear Taylor's
@@ -417,7 +417,7 @@ function IntegradorN(cond_ini, tf, p=28)
             eval(expr5)
             eval(expr6)
         end
-        
+
         hs = Float64[]
         #Calculamos todas las h's y las almacenamos en hs
         for j in 1:N
@@ -462,7 +462,7 @@ function IntegradorN(cond_ini, tf, p=28)
             eval(expr3)
             eval(expr4)
             eval(expr5)
-            eval(expr6)   
+            eval(expr6)
         end
         #Ahora hay que agregar ésta información a los vectores de posición y velocidad
         for j in 1:N
@@ -473,7 +473,7 @@ function IntegradorN(cond_ini, tf, p=28)
         end
         ts = push!(ts, t)
     end
-    
+
     #Ahora, sólo queda acomodar toda la información de forma adecuada y hacer que el programa regrese esos datos
     for j in 1:N
         expr = :($(n_c(j)) = typeof($(n_pj(j)))[$(n_pj(j)), $(n_vj(j))])
@@ -498,7 +498,7 @@ function Energia_3(c1, c2, c3, masas)
     m1 = masas[1]
     m2 = masas[2]
     m3 = masas[3]
-    
+
     ϵ = zeros(length(vc1))
     for j in 1:length(ϵ)
         ϵ1 = m1*(0.5*norm(vc1[j])^2 - m2/norm(pc2[j] - pc1[j]) - m3/norm(pc3[j] - pc1[j]))
@@ -509,4 +509,134 @@ function Energia_3(c1, c2, c3, masas)
     ϵ
 end
 
+function Integrador_Restringido(masas, p0, v0, tf::Float64, p=28)
 
+
+
+    #Para ahorrar al momento de escribir los nombres se indicarán así: pi donde i es el número de cuerpo,
+    # y p inidica si es posición o velocidad
+    #Creamos los arreglos con los tiempos y las soluciones
+    v1 = typeof(zeros(Float64, 3))[p0]
+
+    p1 = typeof(zeros(Float64, 3))[v0]
+
+    ts = Float64[0.] #arreglo inicial para los tiempos
+
+    m1 = masas[1]
+    m2 = masas[2]
+
+    M = m1 + m2
+    mu = m2 / M
+    alpha = m1 / M
+
+    t = 0.
+    h = 1
+
+    while t <= tf && h > 1e-8
+        #Creo arreglos de cada parámetro
+        arr_x1 = Float64[p1[end][1]]
+        arr_y1 = Float64[p1[end][2]]
+        arr_z1 = Float64[p1[end][3]]
+
+        arr_vx1 = Float64[v1[end][1]]
+        arr_vy1 = Float64[v1[end][2]]
+        arr_vz1 = Float64[p1[end][3]]
+
+
+        #Creo Taylor's para cada parámetro
+        for j in 1:p
+            Taylor_arr_x1 = Taylor1(arr_x1)
+            Taylor_arr_y1 = Taylor1(arr_y1)
+            Taylor_arr_z1 = Taylor1(arr_z1)
+
+            Taylor_arr_vx1 = Taylor1(arr_vx1)
+            Taylor_arr_vy1 = Taylor1(arr_vy1)
+            Taylor_arr_vz1 = Taylor1(arr_vz1)
+
+            #@show Taylor_arr_x1
+            ##Definimos la operación de las 6 ecs de movimiento.
+
+
+            r1 = ((Taylor_arr_x1 - mu)^2 + (Taylor_arr_y1)^2 + (Taylor_arr_vz1)^2)
+            r2 = ((Taylor_arr_x1 + alpha)^2 + (Taylor_arr_y1)^2 + (Taylor_arr_vz1)^2)
+            #@show r12, r3, r23
+
+            #Para el cuerpo
+            fx1 = 2*Taylor_arr_vx1 - (alpha / (Taylor_arr_x1 - mu) - (mu*(Taylor_arr_x1 + alpha)/r1^3))
+            fy1 = -2*Taylor_arr_vy1 + (1 - (alpha / r1^3))*Taylor_arr_y1
+            fz1 = - ((alpha/(r1^3)) + (mu / (r2^3)))*Taylor_arr_z1
+
+            #Incluimos los nuevos coeficientes
+            arr_x1 = push!(arr_x1, Taylor_arr_vx1.coeffs[j]/j)
+            arr_y1 = push!(arr_y1, Taylor_arr_vy1.coeffs[j]/j)
+            arr_z1 = push!(arr_z1, Taylor_arr_vz1.coeffs[j]/j)
+
+            arr_vx1 =push!(arr_vx1, fx1.coeffs[j]/j)
+            arr_vy1 =push!(arr_vy1, fy1.coeffs[j]/j)
+            arr_vz1 =push!(arr_vz1, fz1.coeffs[j]/j)
+
+        end
+        #Hacemos Taylor de todos los arreglos finales
+        Taylor_arr_x1 = Taylor1(arr_x1)
+        Taylor_arr_y1 = Taylor1(arr_y1)
+        Taylor_arr_z1 = Taylor1(arr_z1)
+
+        Taylor_arr_vx1 = Taylor1(arr_vx1)
+        Taylor_arr_vy1 = Taylor1(arr_vy1)
+        Taylor_arr_vz1 = Taylor1(arr_vz1)
+
+
+
+        #Calculamos todas las h's posibles
+        h1 = paso_int(Taylor_arr_x1)
+
+        h2 = paso_int(Taylor_arr_y1)
+
+        h3 = paso_int(Taylor_arr_z1)
+        h4 = paso_int(Taylor_arr_z2)
+
+        h5 = paso_int(Taylor_arr_vx1)
+
+        h6 = paso_int(Taylor_arr_vz1)
+
+
+
+        #Elegimos el h más pequeño
+        h = min(h1, h2, h3, h4, h5, h6)
+        t += h
+        #Calculamos el siguiente paso usando Horner
+        x1 = evaluate(Taylor_arr_x1, h)
+
+        y1 = evaluate(Taylor_arr_y1, h)
+
+        z1 = evaluate(Taylor_arr_z1, h)
+
+        vx1 = evaluate(Taylor_arr_vx1, h)
+
+        vy1 = evaluate(Taylor_arr_vy1, h)
+
+        vz1 = evaluate(Taylor_arr_vz1, h)
+
+
+        #Revisamos que no salgan singularidades
+
+
+        #Creamos vectores para la posicion y la velocidad de cada cuerpo
+        p1_temp = Float64[x1, y1, z1]
+
+        v1_temp = Float64[vx1, vy1, vz1]
+
+
+        #Agregamos nueva información de tiempos, posiciones y velocidades
+        p1 = push!(p1, p1_temp)
+
+        v1 = push!(v1, v1_temp)
+
+        ts = push!(ts, t)
+    end
+    #Creamos arreglos con toda la información de los cuerpos
+    cuerpo1 = typeof(p1)[p1, v1]
+
+
+    ts, cuerpo1
+end
